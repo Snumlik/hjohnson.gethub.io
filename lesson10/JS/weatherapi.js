3a3632c72442444c8e6145963436be2b09339634
Snumlik <hannah.dudley@gmail.com>
2019-11-21 21:15:37 -0700

    Update weatherapi.js

--- lesson10/JS/weatherapi.js
+++ lesson10/JS/weatherapi.js
@@ -1,4 +1,4 @@
-const apiURL= "https://api.openweathermap.org/data/2.5/weather?id=2172797&appid=b6907d289e10d714a6e88b30761fae22"
+const apiURL= "https://api.openweathermap.org/data/2.5/weather?id=2172797&APPID=b6907d289e10d714a6e88b30761fae22"
 fetch(apiURL)
     .then{(response)=> response.json()}
     .then{(jsObject)=>{
