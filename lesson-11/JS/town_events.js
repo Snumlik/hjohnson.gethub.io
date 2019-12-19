40a74825e5a51802a5f8f484490556fb2e851cba
zerix807 <alan.turtleboy@gmail.com>
2019-12-19 14:31:07 -0700

    town events for lesson11

--- lesson-11/JS/town_events.js
+++ lesson-11/JS/town_events.js
@@ -1,3 +1,15 @@
+if (document.title == "Preston") {
+    var city = "Preston";
+}
+
+if (document.title == "Soda Springs") {
+    var city = "Soda Springs";
+}
+
+if (document.title == "Fish Haven") {
+    var city = "Fish Haven";
+}
+
 const requestURL = 'JS/towndata.json';
 fetch(requestURL)
     .then(function (response) {
@@ -6,31 +18,18 @@ fetch(requestURL)
     .then(function (jsonObject) {
         const towns = jsonObject['towns'];
         for (let i = 0; i < towns.length; i++) {
-            if (towns[i].name == 'Fish Haven' || towns[i].name == 'Preston' || towns[i].name == 'Soda Springs') {
+            if (towns[i].name == city) {
                 let card = document.createElement('section');
                 let text = document.createElement('div');
-                let name = document.createElement('h2');
-                let motto = document.createElement('h3');
-                let year = document.createElement('p');
-                let population = document.createElement('p');
-                let rain = document.createElement('p');
-                let image = document.createElement('img');
-                name.textContent = towns[i].name;
-                motto.textContent = '"' + towns[i].motto + '"';
-                year.textContent = 'Year Founded: ' + towns[i].yearFounded;
-                population.textContent = 'Current Population: ' + towns[i].currentPopulation;
-                rain.textContent = 'Annual Rain Fall: ' + towns[i].averageRainfall;
-                image.setAttribute('src', 'images/' + towns[i].photo);
-                image.setAttribute('alt', towns[i].name + ' ' + towns[i].motto);
-    
-                text.appendChild(name);
-                text.appendChild(motto);
-                text.appendChild(year);
-                text.appendChild(population);
-                text.appendChild(rain);
+                for (let j = 0; j < towns[i].events.length; j++) {
+                    let event = document.createElement('p');
+                    event.textContent += towns[i].events[j];
+                    text.appendChild(event);
+                }
+
                 card.appendChild(text);
-                card.appendChild(image);
-                document.querySelector('div.towns').appendChild(card);
+
+                document.querySelector('p.eventInfo').appendChild(card);
             }
         }
     });
